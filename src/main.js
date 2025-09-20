7b0abf64387dc8c5d8a551722cba2bca4650e7c3
Scvrlae <scvrlae.vivalavida@gmail.com>
2025-09-20 13:51:12 +0200

    Command loading into client cache

--- src/main.js
+++ src/main.js
@@ -1,10 +1,36 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, ActivityType, Collection } = require("discord.js");
 const { Token }  = require("./data/secrets/token.json");
+
+const fs = require("node:fs");
+const path = require("node:path");
+
+const { Status, PresenceType, Presence } = require("./data/config.json");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, async readyClient => {
-       
-})
+function onStart() {
+       client.commands = new Collection();
 
-client.login(Token)
+       const commandsPath = path.join(__dirname, "commands");
+       const directoryToLoad = fs.readdirSync(commandsPath);
+
+       for (const file of directoryToLoad) {
+              const filePath = path.join(commandsPath, file);
+              const command = require(filePath);
+
+              client.commands.set(command.data.name, command);
+       }
+
+       client.login(Token);
+}
+
+client.once(Events.ClientReady, async readyClient => {
+       readyClient.user.setPresence({
+              activities: [{name: Presence, type: ActivityType[PresenceType]}],
+              status: Status
+       });
+
+       console.log("Client ready (temporary log)");
+});
+
+onStart();
